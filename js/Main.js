c70961c0991202ae5ff6315c628cb37d34e882a1
LUIS FELIPE RESTREPO <112680688+codingnow2022@users.noreply.github.com>
2023-06-23 06:04:23 -0500

    Add files via upload

---
+++ js/Main.js
@@ -0,0 +1,10 @@
+
+function activarMenu() {
+  let menuOculto = document.getElementById("menu-oculto");
+  let body = document.querySelector("body");
+
+  body.classList.toggle("ovfh")
+  menuOculto.classList.toggle("active");
+}
+
+
